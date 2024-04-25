fb28cfed51170d4efdfd1dfb1abaea1ac41fef32
ameall <83421421+ameall@users.noreply.github.com>
2024-04-25 00:44:45 -0400

    Delete Test_Runs directory
    Prep for merge

--- Test_Runs/FIR_Working/FIR_Test_Vitis/solution1/.autopilot/db/coregen/control.slave.tcl
+++
@@ -1,4 +0,0 @@
-dict set slaves control {ports {coefs {type i_ap_none width 64}} mems {} has_ctrl 0}
-set datawidth 32
-set addrwidth 64
-set intr_clr_mode TOW
--- Test_Runs/FIR_Working/FIR_Test_Vitis/solution1/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,9 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "filt","ID" : "0","Type" : "sequential",
-"SubLoops" : [
-	{"Name" : "VITIS_LOOP_24_1","ID" : "1","Type" : "no",
-	"SubInsts" : [
-	{"Name" : "grp_filt_Pipeline_VITIS_LOOP_56_2_fu_519","ID" : "2","Type" : "sequential",
-			"SubLoops" : [
-			{"Name" : "VITIS_LOOP_56_2","ID" : "3","Type" : "pipeline"},]},]},]
-}]}
